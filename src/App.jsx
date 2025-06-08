0f38cb740c0469dca55173fcf439a2f34e36753e
vaijaaaaa <vaijuwalker111@gmail.com>
2025-06-08 22:18:50 +0530

    Refactor routing in App component to standardize paths and improve structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
-
+import FeaturesPage from './Components/FeaturesPage';
+import HowItWorksPage from './Components/HowItWorksPage';
+import TruLensContactPage from './Components/TruLensContactPage';
+import TruLensDownloadPage from './Components/TruLensDownloadPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/features" element={<FeaturesPage />} />
+        <Route path="/how-it-works" element={<HowItWorksPage />} />
+        <Route path="/download" element={<TruLensDownloadPage />} />
+        <Route path="/contact" element={<TruLensContactPage />} />
       </Routes>
     </Router>
   );
