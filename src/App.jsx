20f71237ec579b97d3b8131affd22035b072acd8
vaijaaaaa <vaijuwalker111@gmail.com>
2025-06-08 22:29:01 +0530

    Update title in index.html and enhance routing in App component for improved navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Components/Home';
 import FeaturesPage from './Components/FeaturesPage';
 import HowItWorksPage from './Components/HowItWorksPage';
@@ -9,6 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/home" element={<Home />} />
         <Route path="/features" element={<FeaturesPage />} />
         <Route path="/how-it-works" element={<HowItWorksPage />} />
