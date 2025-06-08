4f3bca7e14ac8ebebf5e5f103161e51cdae111b4
vaijaaaaa <vaijuwalker111@gmail.com>
2025-06-08 21:56:09 +0530

    updated home

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,9 @@ import Navbar from './Components/Navbar';
 function App() {
   return (
     <Router>
-      <div className="flex justify-center items-center h-screen bg-green-500">
-        <div>
-          <Navbar />
-          <Routes>
-            <Route path="/home" element={<Home/>} />
-            <Route path="/about" element={<h1 className="text-white text-3xl">About Page</h1>} />
-          </Routes>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
